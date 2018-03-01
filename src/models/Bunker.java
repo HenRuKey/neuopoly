49e7e3fcf2c8b5fe92b89aa47b3132922ea4f7fd
Silver-Talen <silver_talen@hotmail.com>
2018-03-01 16:09:34 -0700

    change Go to GoStipend / add Bunker, GoToServerRoom, ServerRoom,

--- src/models/Bunker.java
+++ src/models/Bunker.java
@@ -2,4 +2,22 @@ package models;
 
 public class Bunker {
 
+	private int value;
+	
+	public Bunker() {
+		
+	}
+	
+	public void payPlayer(Player player) {
+		
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
 }
