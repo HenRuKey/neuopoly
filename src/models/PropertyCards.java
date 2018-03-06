1a9286e6b8e4128aecef475cbf65a3de47598e46
Ckknudsen <ckknudsen1@hotmail.com>
2018-03-06 15:38:54 -0700

    This will serialize the properties on the first start up

---
+++ src/models/PropertyCards.java
@@ -0,0 +1,24 @@
+package models;
+
+public class PropertyCards {
+
+	Property[] properties = new Property[22];
+	
+	public PropertyCards() {
+		
+		properties[0] = new Property("Purple", "Bridges", 60, 30, 2, 250, "Houses $50 each, Hotel $50 + 4 Houses");
+		properties[1] = new Property("Purple", "City Station", 60, 30, 4, 450, "Houses $50 each, Hotel $50 + 4 Houses");
+		properties[2] = new Property("Light Blue", "Mercer", 100, 50, 6, 550, "Houses $50 each, Hotel $50 + 4 Houses");
+		properties[3] = new Property("Light Blue", "Elevate", 100, 50, 6, 550, "Houses $50 each, Hotel $50 + 4 Houses");
+		properties[4] = new Property("Light Blue", "Essex", 120, 60, 8, 600, "Houses $50 each, Hotel $50 + 4 Houses");
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
